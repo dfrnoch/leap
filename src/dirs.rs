97113732963e1576963fdf2fe3e2cb0700379c24
lnxcz <david.frnoch@creativehill.cz>
2022-09-30 10:01:18 +0200

    fix: :bug: fix fodler problems

--- src/dirs.rs
+++ src/dirs.rs
@@ -2,11 +2,16 @@ use std::env;
 use std::path::PathBuf;
 
 pub fn get_dir(var: &str, join: &str) -> Option<PathBuf> {
-    env::var_os(var).map(|path| PathBuf::from(path).join(join))
+    let dir = env::var_os(var).map(|path| PathBuf::from(path).join(join));
+    if let Some(ref dir) = dir {
+        std::fs::create_dir_all(dir).unwrap();
+    }
+    dir
 }
 
 pub fn cache_dir() -> Option<PathBuf> {
     get_dir("HOME", ".cache/leap")
+
 }
 
 pub fn data_dir() -> Option<PathBuf> {
