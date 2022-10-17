de709a800039072d2d3cf038d6231112d0c94434
lnxcz <david.frnoch@creativehill.cz>
2022-10-17 14:28:49 +0200

    feat: :sparkles: remove

--- src/appimage/remove.rs
+++ src/appimage/remove.rs
@@ -1,3 +1,20 @@
-pub fn remove() {
-    todo!()
+use std::{fs, error::Error};
+
+use crate::dirs::bin_dir;
+
+use super::AppImage;
+
+impl AppImage {
+    pub fn remove(&mut self) -> Result<(), Box<dyn Error>> {
+        let path = &self.path;
+        let app_path = path.join(format!("{}.AppImage", self.name));
+
+        log::info!("Removing AppImage");
+        fs::remove_file(&app_path)?;
+
+        log::info!("Removing symlink");
+        fs::remove_file(&bin_dir().join(&self.name))?;
+
+        Ok(())
+    }
 }
